204858bb9d51c468eb3664ff8ad4b9aaceee749e
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-08-18 11:33:50 +0530

    Styling

---
+++ App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Here are some boxes of different colours
+        </Text>
+        <View style={styles.cyanBox}>
+          <Text style={styles.text}>Cyan #2aa198</Text>
+        </View>
+        <View style={styles.blueBox}>
+          <Text style={styles.text}>Blue #268bd2</Text>
+        </View>
+        <View style={styles.magentaBox}>
+          <Text style={styles.text}>Magenta #d33682</Text>
+        </View>
+        <View style={styles.orangeBox}>
+          <Text style={styles.text}>Orange #cb4b16</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  cyanBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#2aa198',
+  },
+  blueBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#268bd2',
+  },
+  magentaBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#d33682',
+  },
+  orangeBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#cb4b16',
+  },
+});
+
+export default App;
